d679dbeffd80869ab12e1d152865efebf157b835
Thinh Bui <Thinh Bui@DESKTOP-T132>
2025-06-15 23:03:16 +0700

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,7 +26,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://thinhbui.github.io",
+	site: "https://thinhbuicong.github.io",
 	base: "/",
 	trailingSlash: "always",
 	integrations: [
